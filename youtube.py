43b5dd19eeababfe753198b8df3afb033d6e4793
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-02-01 09:00:38 -0800

    commit

--- youtube.py
+++ youtube.py
@@ -7,6 +7,7 @@ import yaml
 import re
 import time
 import traceback as tb
+from telegram_util import matchKey
 
 def getList(pivot, text):
     for m in re.finditer(pivot, text):
@@ -16,11 +17,21 @@ def getList(pivot, text):
         yield tmp[:x]
 
 
-def videoFilter(title, v_len, _):
+def videoFilter(title, v_len, author, _, raw):
     if '镇魂' in title and v_len > 60 * 20:
+        return False
+    if matchKey(title, ['丁毅', '聰明的一休', '準提咒']):
+        return False
+    if matchKey(author, ['sunfirekiss', '丁毅', 'YaleUniversity']):
+        return False
+    if matchKey(raw, ['Human Behavioral Biology']):
         return False
     return True
 
+def getPiece(b, e, text):
+    if text.find(b) == -1 or text.find(e) == -1:
+        return ''
+    return text[text.find(b) + len(b): text.find(e)].strip()[:-1]
 
 with open('setting.yaml') as f:
     setting = yaml.load(f, Loader=yaml.FullLoader)
@@ -33,6 +44,9 @@ list_pivot = 'playlist\?list='
 video_pivot = '"videoId":"'
 length_pivot = '"lengthSeconds":"'
 title_pivot = '","title":"'
+author_pivot = ',"author":"'
+json_pivot = 'window["ytInitialData"] = '
+json_end = 'window["ytInitialPlayerResponse"]'
 
 lists_html = cached_url.get(lists_url)
 
@@ -45,24 +59,28 @@ with open('videos.txt') as f:
     videos = f.read().split()
 
 video_info = []
-
+titles = set()
 for v in set(videos):
     try:
         v_html = cached_url.get(video_url + v)
         v_len = int(next(getList(length_pivot, v_html)))
         title = next(getList(title_pivot, v_html))
-        video_info.append((title, v_len, video_url + v))
+        if title in titles:
+            continue
+        titles.add(title)
+        author = next(getList(author_pivot, v_html))
+        video_info.append((title, v_len, author, video_url + v, v_html))
         with open('video_info.txt', 'a') as f:
-            f.write('%s\t%d\t%s\n' % (title, v_len, video_url + v))
+            f.write('%s\t%d\t%s\t%s\n' % (title, v_len, author, video_url + v))
     except Exception as e:
-        pass
-        # print(e)
-        # tb.print_exc()
+        # pass
+        print(e)
+        tb.print_exc()
 
 
-video_info = [x for x in video_info if videoFilter(**x)]
+video_info = [x for x in video_info if videoFilter(*x)]
 video_info = sorted(video_info, key=lambda x: x[1])
 with open('video_info.txt', 'w') as f:
-    f.write('Title\tLength\tUrl\n')
+    f.write('Title\tLength\tAuthor\tUrl\n')
     for x in video_info:
-        f.write('%s\t%d\t%s\n' % x)
+        f.write('%s\t%d\t%s\t%s\n' % x[:-1])
